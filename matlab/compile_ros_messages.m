91525ba6a915c6dc43b6764e90ab77a73db190e6
Bardo91 <19bardo91@gmail.com>
2019-03-04 01:18:05 +0100

    Initialized with basic matlab code

---
+++ matlab/compile_ros_messages.m
@@ -0,0 +1,15 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% Install Robotic addon 
+%      >> roboticsAddons
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Define the path to grvc-ual workspace 
+path = '/home/bardo91/programming/catkin_grvc/src/grvc-ual';
+
+rosgenmsg(path);
+
+display('---------------------------------------');
+display('Please follow the instructions')
+display('---------------------------------------');
