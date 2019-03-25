ed3ddb7af97c35b27fc927bb0db83bdc653a00f6
Bardo91 <19bardo91@gmail.com>
2019-03-26 00:10:44 +0100

    Added take off and land functions

--- matlab/debugging/MobileRobot.m
+++ matlab/debugging/MobileRobot.m
@@ -8,6 +8,24 @@ classdef MobileRobot < handle
             obj.poseSubscriber = rossubscriber('/ual/pose', @obj.callbackPose);  
             figure(100);
         end
+        
+        function takeoffResp = takeoff(obj, altitude)
+            clientTakeoff = rossvcclient('/ual/take_off');  
+            takeoffReq = rosmessage(clientTakeoff); 
+            takeoffReq.Blocking = 1;
+            takeoffReq.Height = altitude;
+            takeoffResp = call(clientTakeoff,takeoffReq);
+            pause(3)
+        end
+        
+        function landResp = land(obj)
+            clientLand = rossvcclient('/ual/land');  
+            landReq = rosmessage(clientLand); 
+            landReq.Blocking = 1;
+            landResp = call(clientLand,landReq);
+            pause(3)
+        end
+        
         function  callbackPose(obj, src, msg) 
             obj.pose = msg.Pose.Position;
             figure(100);
