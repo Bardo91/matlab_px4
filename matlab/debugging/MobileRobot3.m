a12d9e28f852529114f4ef5fdeb01d1d254adde2
EmaSava <emanuela.savastano@libero.it>
2019-05-01 22:14:37 +0200

    MobileRobot3
    Ultimate version of the control function

--- matlab/debugging/MobileRobot3.m
+++ matlab/debugging/MobileRobot3.m
@@ -13,7 +13,7 @@ classdef MobileRobot3 < handle
             % Parameters
             pose;
             speed;
-            waypoints = ([3 10 11; 3 15 10; 1 15 26]);
+            waypoints = [];
             trajectory_pos = [];
             trajectory_speed = [];
             N_traj=[];
@@ -32,7 +32,7 @@ classdef MobileRobot3 < handle
             obj.poseReadSubscriber = rossubscriber('/ual/pose', @obj.callbackReadPose);  
             obj.poseWpSubscriber = rossubscriber('/ual/pose', @obj.callbackWpPose);  
             obj.poseReplanSubscriber = rossubscriber('/ual/pose', @obj.callbackReplanPose);  
-            %obj.speedSubscriber = rossubscriber('/ual/velocity', @obj.callbackSpeed);  
+            obj.speedSubscriber = rossubscriber('/ual/velocity', @obj.callbackSpeed);  
             obj.speedPublisher = rospublisher('/ual/set_velocity','geometry_msgs/TwistStamped');
             obj.speed_msg = rosmessage(obj.speedPublisher);
         end
@@ -55,7 +55,7 @@ classdef MobileRobot3 < handle
             pause(3)
         end
         
-        function  callbackReadPose(obj, src, msg) 
+        function  callbackReadPose(obj, ~, msg) 
             obj.pose = msg.Pose.Position;
              figure(1)
 %             subplot(2,1,1)
@@ -63,9 +63,9 @@ classdef MobileRobot3 < handle
             hold on
         end
         
-        function  callbackWpPose(obj, src, msg) 
-%            obj.pose = msg.Pose.Position;
-           [dim nwps]=size(obj.waypoints);
+        function  callbackWpPose(obj, ~, msg) 
+             obj.pose = msg.Pose.Position;
+           [~ , obj.nwps]=size(obj.waypoints);
            
 %            plot3(obj.waypoints(1,1), obj.waypoints(2,1), obj.waypoints(3,1), 'o')
 %            hold on
@@ -74,35 +74,38 @@ classdef MobileRobot3 < handle
 %            plot3(obj.waypoints(1,3), obj.waypoints(2,3), obj.waypoints(3,3), 'o')
 %            hold on
            
-             for i=1:nwps
+             for i=1:obj.nwps
                    dist_wayps = [obj.pose.X obj.pose.Y obj.pose.Z] - ... 
                   [ obj.waypoints(1,i) obj.waypoints(2,i) obj.waypoints(3,i) ];
                  
-                 if norm(dist_wayps) < 0.4
-                   obj.waypoints =obj.waypoints(:,i+1:nwps);
+                 if norm(dist_wayps) < 0.6
+                   obj.waypoints =obj.waypoints(:,i+1:obj.nwps);
                  end
              end 
         end
          
         
-        function  callbackSpeed(obj, src, msg) 
+        function  callbackSpeed(obj, ~, msg) 
                 obj.speed = msg.Twist.Linear;
         end
         
         
         function computeTrajectory(obj)
             num_axes         = 3;
-            [dim nwps ]=size(obj.waypoints);
-            num_trajectories = nwps; %number of waypoint
-            State_start      = [obj.pose.X obj.speed.X 0;... 
-                                obj.pose.Y obj.speed.Y 0;...
+            [~ , obj.nwps ]=size(obj.waypoints);
+            num_trajectories = obj.nwps; %number of waypoint
+
+            State_start      = [obj.pose.X obj.speed.X 0; 
+                                obj.pose.Y obj.speed.Y 0;
                                 obj.pose.Z obj.speed.Z 0];
-            Waypoints = [];
+                            
+                Waypoints=[];
             
-            for i=1:nwps
+            for i=1:obj.nwps
+               
                 Waypoints(:,:,i) = [    obj.waypoints(1,i)  0.0  0.0  0.0  0.0; ...
                                         obj.waypoints(2,i)  0.0  0.0  0.0  0.0; ...
-                                        obj.waypoints(3,i)  0.0 0.0 0.0 0.0];
+                                        obj.waypoints(3,i)  0.0  0.0  0.0  0.0];
             end
             
             V_max            =  1.0*ones(num_axes,num_trajectories);
@@ -136,7 +139,7 @@ classdef MobileRobot3 < handle
 
             ts_rollout = 0.06; %0.01
             T_rollout = max(sum(T_waypoints,2));
-            [P,V,A,J] = rollout(State_start(:,1),State_start(:,2),State_start(:,3)+A_global*b_comp_global,J_setp_struct,T_rollout,ts_rollout);
+            [P,V,~,~] = rollout(State_start(:,1),State_start(:,2),State_start(:,3)+A_global*b_comp_global,J_setp_struct,T_rollout,ts_rollout);
 
             obj.trajectory_pos = P;
             obj.trajectory_speed = V;  
@@ -146,65 +149,57 @@ classdef MobileRobot3 < handle
         end  
         
         
-          function  callbackReplanPose(obj,src, msg) 
-            if obj.flying == true
-%            PREGUNTA:NECESITO TODAS LAS LINEAS COMENTADAS SI LAS VARIABLES SON OBJECTOS?
-            obj.pose = msg.Pose.Position;
-%            
-%           COMPUTE TRAJECTORY
-%             
-            computeTrajectory(obj);
+         function  callbackReplanPose(obj,~, msg) 
+            if obj.flying == true && obj.nwps > 0
+            pose_r = msg.Pose.Position;
 
-%            PLOT OF THE FIRST TRAJECTORY
+            %calcola l'errore
+            obj.error(obj.speed_index,:) = [pose_r.X pose_r.Y pose_r.Z] - ... 
+                                           [obj.trajectory_pos(1).signals.values(obj.speed_index,1) obj.trajectory_pos(2).signals.values(obj.speed_index,1) obj.trajectory_pos(3).signals.values(obj.speed_index,1)];
+
+                 
+                 if norm(obj.error(obj.speed_index,:)) > 0.6
+                          
+                      computeTrajectory(obj) 
+                         
+                      disp('errore maggiore di 0.6')
+                         
+                      obj.speed_index = 1;
+                         
+                end
+             end
+          end
+               
+   
+           function moveToWaypoint(obj, waypoints)
+               
+           obj.waypoints = waypoints;
+           
+%           COMPUTE TRAJECTORY 
+            computeTrajectory(obj);
+            
+            
+%           PLOT OF THE FIRST TRAJECTORY => VERIFICA FUNZIONAMENTO 
             plot3(obj.trajectory_pos(1).signals.values,...
                   obj.trajectory_pos(2).signals.values,... 
                   obj.trajectory_pos(3).signals.values);
             hold on 
-           
-%              V = obj.trajectory_speed;
-%              P = obj.trajectory_pos;
-% 
-%              obj.waypoints = waypoints; %E' RIDONDNTE QUESTA INFORMAZIONE?
-            [dim nwps ]=size(obj.waypoints);
-            
-            hasFinishedTraj  = false;
-           
-            while nwps > 0 && ~hasFinishedTraj
-              for i=1: obj.N_traj
-                  tic
-                  %calcola l'errore
-                  obj.error(obj.counter_step,:) = [obj.pose.X obj.pose.Y obj.pose.Z] - ... 
-                              [obj.trajectory_pos(1).signals.values(i,1) obj.trajectory_pos(2).signals.values(i,1) obj.trajectory_pos(3).signals.values(i,1)];
 
-                  if norm(obj.error(obj.counter_step,:)) > 0.6
-                     computeTrajectory(obj) 
-                     break 
-                  end
-                  obj.counter_step = obj.counter_step+1; 
-                  obj.speed_index = i;
-                  moveToWaypoint(obj)
-                  incT = toc;
-%                  pause(10^-09)
-                                          
-                  if hasFinishedTraj == obj.N_traj
-                     hasFinishedTraj = true;
-                  end
-              end
-            end     
-             end
-         end
-               
-        
-        function moveToWaypoint(obj)  
+           while obj.speed_index < obj.N_traj
+               tic   
+               obj.speed_msg.Twist.Linear.X= obj.trajectory_speed(1).signals.values(obj.speed_index,1);
+               obj.speed_msg.Twist.Linear.Y = obj.trajectory_speed(2).signals.values(obj.speed_index,1);
+               obj.speed_msg.Twist.Linear.Z = obj.trajectory_speed(3).signals.values(obj.speed_index,1);
+
+               send(obj.speedPublisher, obj.speed_msg)
+               obj.speed_index = obj.speed_index+1;
+               disp('ha inviato velocità')
+               inct=toc;
+               if(inct<0.6)
+                  pause(0.033); %nella prova di volo poni 0.06;
+               end 
            
-%            display('-----------------------------------------------------------------------------------------')
-           obj.speed_msg.Twist.Linear.X= obj.trajectory_speed(1).signals.values(obj.speed_index,1);
-           obj.speed_msg.Twist.Linear.Y = obj.trajectory_speed(2).signals.values(obj.speed_index,1);
-           obj.speed_msg.Twist.Linear.Z = obj.trajectory_speed(3).signals.values(obj.speed_index,1);
-                   
-           send(obj.speedPublisher, obj.speed_msg)
-                  
-           pause(10^-09)
+           end 
                   
         end
     end   
